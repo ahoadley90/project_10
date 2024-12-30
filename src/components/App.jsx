19dac94f5fd138f93ecb93f3ae22f094643c66fb
AmandaKim <akimmets@gmail.com>
2024-12-29 19:46:57 -0600

    corrected to connect import for Header and Main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,8 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
+      <Header />
+      <Main />
       <h1>Hello Vite!</h1>
       <p>This is your first Vite project</p>
     </>
